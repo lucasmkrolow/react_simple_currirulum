28d9cfeb35750e22b8fe7efc84f15120b91640d3
lucasmkrolow <lucaskrolowcba@gmail.com>
2022-05-10 10:09:50 -0400

    Alteração nos ícones do painel esquerdo

--- src/components/leftSide/leftSide.js
+++ src/components/leftSide/leftSide.js
@@ -3,6 +3,14 @@ import "./leftSide.css";
 import Formacao from "./formacao/formacao";
 import Habilidades from "./habilidades/habilidades";
 import Idiomas from "./idiomas/idiomas";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faGraduationCap,
+  faBrain,
+  faLanguage
+} from "@fortawesome/free-solid-svg-icons";
+library.add(faGraduationCap, faBrain, faLanguage);
 
 export default function Leftside() {
   return (
@@ -11,6 +19,11 @@ export default function Leftside() {
         <Formacao />
         <Habilidades />
         <Idiomas />
+      </div>
+      <div className="lefticons">
+        <FontAwesomeIcon icon="fa-solid fa-graduation-cap" />
+        <FontAwesomeIcon icon="fa-solid fa-brain" />
+        <FontAwesomeIcon icon="fa-solid fa-language" />
       </div>
     </div>
   );
