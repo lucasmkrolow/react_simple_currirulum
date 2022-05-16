ae2c1605824a4042dec0eb00e8b3d6cecc32cedf
lucasmkrolow <lucaskrolowcba@gmail.com>
2022-05-16 10:40:51 -0400

    Fixed SW

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,16 +1,20 @@
+import { precacheAndRoute } from "workbox-precaching";
+
+precacheAndRoute(self.__WB_MANIFEST);
+
 var CACHE_NAME = "sw-test-v1";
 var urlsToCache = ["/"];
 
-self.addEventListener("install", function(event) {
+self.addEventListener("install", function (event) {
   //perform install steps
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
+    caches.open(CACHE_NAME).then(function (cache) {
       console.log("Opened cache");
       return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener("activate", function(event) {
+self.addEventListener("activate", function (event) {
   console.log("service worker is activated");
 });
